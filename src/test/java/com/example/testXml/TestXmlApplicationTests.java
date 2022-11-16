6a5c907c78a4839aaf053cccb9afb803e899b9c5
MaxiRage <iarostiuk@ya.ru>
2022-11-16 12:49:42 +0300

    Initial commit

---
+++ src/test/java/com/example/testXml/TestXmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testXml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestXmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
